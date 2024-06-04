2611d05b08f925a8d099e0825d8ec3b6821b5e67
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2024-06-04 07:30:53 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <>
       <nav className="sticky top-0 z-10 border-b border-gray-200 bg-slate-100 bg-opacity-30 backdrop-blur-lg backdrop-filter">
@@ -12,7 +15,85 @@ const Navbar = () => {
                 jcz.dev
               </a>
             </div>
-            {/* TODO: Add mobile menu */}
+
+            {/* MOBILE MENU */}
+            {/* <div
+              onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+              className="md:hidden"
+            >
+              {mobileMenuOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 h-9 w-9 rounded-sm bg-slate-100 p-2 outline outline-1 outline-slate-300 hover:bg-slate-200 hover:bg-opacity-50 hover:outline-2 hover:outline-slate-400"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 h-9 w-9 rounded-sm bg-slate-200 p-2 outline outline-2 outline-slate-400"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              )}
+
+              {mobileMenuOpen && (
+                <div className="[&>*]:border-1 absolute left-0 right-0 top-14 border-b border-gray-200 bg-slate-300 drop-shadow-sm md:hidden [&>*]:border-b [&>*]:border-slate-50">
+                  <NavLink
+                    to="/"
+                    className="block py-3 text-center text-slate-700 hover:bg-gray-100"
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    to="/about"
+                    className="block py-3 text-center text-slate-700 hover:bg-gray-100"
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    About
+                  </NavLink>
+                  <NavLink
+                    to="/projects"
+                    className="block py-3 text-center text-slate-700 hover:bg-gray-100"
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    Projects
+                  </NavLink>
+                  <a
+                    href="/JolsonCruz_0524-FE.pdf"
+                    className="block py-3 text-center text-slate-700 hover:bg-gray-100"
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    CV
+                  </a>
+                  <NavLink
+                    to="/contact"
+                    className="block py-3 text-center text-slate-700 hover:bg-gray-100"
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    Contact
+                  </NavLink>
+                </div>
+              )}
+            </div> */}
+
             <div className="hidden md:flex md:space-x-5 md:text-zinc-400">
               <NavLink
                 to="/"
