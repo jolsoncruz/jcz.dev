82d7017343ced57e9db94b30cf96d84079016f29
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2024-08-18 18:36:56 -0400

    Env update

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -18,12 +18,13 @@ const Newsletter = () => {
       from_email: data.email,
     };
 
-    const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-    const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID2;
-    const emailKey = import.meta.env.VITE_EMAILJS_API;
-
     emailjs
-      .send(serviceId, templateId, templateParams, emailKey)
+      .send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID2,
+        templateParams,
+        import.meta.env.VITE_EMAILJS_API,
+      )
       .then(() => {
         console.log("Email sent!");
         // setResult("Thank you for reaching out! 🚀");
